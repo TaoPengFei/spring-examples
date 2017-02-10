aabd706009620bcf60a06a4ec9b3c6d465063144
jason <819151997@163.com>
2017-02-11 00:02:40 +0800

    add java file

---
+++ spring-core/src/main/java/cn/lee/jason/core/di/output/OutputHelper.java
@@ -0,0 +1,14 @@
+package cn.lee.jason.core.di.output;
+
+/**
+ * Created by jason on 17/2/11.
+ */
+public class OutputHelper {
+
+    IOutputGenerator outputGenerator;
+
+    public void setOutputGenerator(IOutputGenerator outputGenerator){
+        this.outputGenerator = outputGenerator;
+    }
+
+}
