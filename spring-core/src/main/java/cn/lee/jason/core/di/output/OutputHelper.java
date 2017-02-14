175a41adec3fa7e0937bf1f7408b89c7ec561da7
jasonlee529 <libo529@gmail.com>
2017-02-14 23:57:25 +0800

    setter

--- spring-core/src/main/java/cn/lee/jason/core/di/output/OutputHelper.java
+++ spring-core/src/main/java/cn/lee/jason/core/di/output/OutputHelper.java
@@ -7,6 +7,10 @@ public class OutputHelper {
 
     IOutputGenerator outputGenerator;
 
+    public void generateOutput() {
+        outputGenerator.generateOutput();
+    }
+
     public void setOutputGenerator(IOutputGenerator outputGenerator) {
         this.outputGenerator = outputGenerator;
     }
