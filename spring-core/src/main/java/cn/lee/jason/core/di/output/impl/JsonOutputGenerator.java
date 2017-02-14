4d6fbdc5ffaa0f9d9eaba647c408cdc53c490c22
jasonlee529 <libo529@gmail.com>
2017-02-15 00:08:54 +0800

    add json impl

---
+++ spring-core/src/main/java/cn/lee/jason/core/di/output/impl/JsonOutputGenerator.java
@@ -0,0 +1,10 @@
+package cn.lee.jason.core.di.output.impl;
+
+/**
+ * Created by jason on 17/2/15.
+ */
+public class JsonOutputGenerator implements CsvOutputGenerator {
+    public void generateOutput() {
+        System.out.println("This is Json Output Generator");
+    }
+}
