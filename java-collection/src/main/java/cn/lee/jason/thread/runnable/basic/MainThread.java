14ea77e4f84f3289ba7ebc85895854e290c9c32e
jasonlee529 <libo529@gmail.com>
2017-02-16 00:12:17 +0800

    add run

---
+++ java-collection/src/main/java/cn/lee/jason/thread/runnable/basic/MainThread.java
@@ -0,0 +1,14 @@
+package cn.lee.jason.thread.runnable.basic;
+
+/**
+ * Created by jason on 17/2/16.
+ */
+public class MainThread {
+
+    public static void main(String[] args) {
+        LiftOff lift = new LiftOff();
+        lift.run();
+
+
+    }
+}
