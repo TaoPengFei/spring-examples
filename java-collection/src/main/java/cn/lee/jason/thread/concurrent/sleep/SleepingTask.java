c0ea56e1f2071aa18fd6e5d112585989649b22e6
jasonlee529 <libo529@gmail.com>
2017-02-18 00:44:48 +0800

    add sleep task

---
+++ java-collection/src/main/java/cn/lee/jason/thread/concurrent/sleep/SleepingTask.java
@@ -0,0 +1,32 @@
+package cn.lee.jason.thread.concurrent.sleep;
+
+import cn.lee.jason.thread.runnable.basic.LiftOff;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by jason on 17/2/18.
+ */
+public class SleepingTask extends LiftOff {
+    @Override
+    public void run() {
+        try {
+            while (countDown-- > 0) {
+                System.out.println(status());
+                TimeUnit.MILLISECONDS.sleep(100L);
+            }
+        } catch (InterruptedException e) {
+            System.err.print("Interrupt");
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            exec.execute(new SleepingTask());
+        }
+        exec.shutdown();
+    }
+}
