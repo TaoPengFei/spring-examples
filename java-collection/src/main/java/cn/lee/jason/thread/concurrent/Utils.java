1c295a7f56bca705dfd0d09bfaf34905eeed29d2
jasonlee529 <libo529@gmail.com>
2017-02-18 23:08:03 +0800

    add thread factory

---
+++ java-collection/src/main/java/cn/lee/jason/thread/concurrent/Utils.java
@@ -0,0 +1,11 @@
+package cn.lee.jason.thread.concurrent;
+
+/**
+ * Created by jason on 17/2/18.
+ */
+public class Utils {
+
+    public static void print(String str) {
+        System.out.println(str);
+    }
+}
