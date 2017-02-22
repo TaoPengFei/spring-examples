6e5f9beb23097261fae925eb8fa90af81f81dd59
jasonlee529 <libo529@gmail.com>
2017-02-23 00:31:55 +0800

    add trylock

---
+++ java-collection/src/main/java/cn/lee/jason/thread/concurrent/lock/AttemptLocking.java
@@ -0,0 +1,61 @@
+package cn.lee.jason.thread.concurrent.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static cn.lee.jason.util.Utils.print;
+
+/**
+ * Created by jason on 17/2/23.
+ */
+public class AttemptLocking {
+
+    private ReentrantLock lock = new ReentrantLock();
+
+    public void untimed() {
+        boolean captured = lock.tryLock();
+        try {
+            print(" tryLock " + captured);
+        } finally {
+            if (captured) {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void timed() {
+        boolean captured = false;
+
+        try {
+            captured = lock.tryLock(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            print(" tryLock(2,TimeUnit.SECONDS) : " + captured);
+        } finally {
+            if (captured) {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final AttemptLocking lal = new AttemptLocking();
+        lal.untimed();
+        lal.timed();
+        new Thread() {
+            @Override
+            public void run() {
+                lal.lock.lock();
+            }
+
+            {
+                setDaemon(true);
+            }
+        }.start();
+        Thread.yield();
+        lal.untimed();
+        lal.timed();
+    }
+}
