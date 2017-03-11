ea0bb2cf9392fa7c659535b0e4720153b68b0c3c
jasonlee <libo529@gmail.com>
2017-03-11 21:04:15 +0800

    add file

--- java-io/src/test/java/cn/lee/jason/io/dicom/TestDcm4chJson.java
+++ java-io/src/test/java/cn/lee/jason/io/dicom/TestDcm4chJson.java
@@ -1,8 +1,10 @@
 package cn.lee.jason.io.dicom;
 
+import java.io.File;
 import java.io.IOException;
+import javax.xml.transform.TransformerConfigurationException;
 
-import org.dcm4che3.tool.dcm2jpg.Dcm2Jpg;
+import org.dcm4che3.tool.dcm2json.Dcm2Json;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +13,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 /**
- * Created by jason on 17/3/10.
+ * Created by jason on 17-3-11.
  */
-public class TestDcm4chImage {
-
+public class TestDcm4chJson {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Test
@@ -31,11 +32,13 @@ public class TestDcm4chImage {
 
     private void doConvert(Resource resource) {
         try {
-            String path = resource.getFile().getPath() + ".jpg";
+            String path = resource.getFile().getPath() + ".json";
             logger.info(path);
-            Dcm2Jpg dcm2Jpg = new Dcm2Jpg();
-            dcm2Jpg.initImageWriter("jpeg", null, null, null, null);
+            Dcm2Json dcm2Json = new Dcm2Json();
+            dcm2Json.convert(resource.getFile(), new File(path));
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
             e.printStackTrace();
         }
     }
