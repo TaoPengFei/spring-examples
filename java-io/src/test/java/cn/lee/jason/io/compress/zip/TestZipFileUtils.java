b3c71df4d0edfb29014f5a416ab0c12755886b0f
jasonlee <libo529@gmail.com>
2017-03-18 23:57:31 +0800

    add test zip

---
+++ java-io/src/test/java/cn/lee/jason/io/compress/zip/TestZipFileUtils.java
@@ -0,0 +1,20 @@
+package cn.lee.jason.io.compress.zip;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+/**
+ * Created by jason on 17-3-18.
+ */
+public class TestZipFileUtils {
+
+    @Test
+    public void test() throws IOException {
+        Resource resource = new ClassPathResource("/dicom/test.zip");
+        ZipFileUtils.decompressZip(resource.getFile().getPath(),resource.getFile().getParent()+"/"+resource.getFilename());
+    }
+}
