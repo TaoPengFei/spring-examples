0db5a22928ff47cec34ae57a5b9ae6600c249a20
jasonlee <libo529@gmail.com>
2017-03-28 17:50:42 +0800

    add pool

---
+++ java-collection/src/main/java/cn/lee/jason/thread/concurrent/demo/pool/ConnectionPool.java
@@ -0,0 +1,52 @@
+package cn.lee.jason.thread.concurrent.demo.pool;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+/**
+ * Created by jason on 17-3-28.
+ */
+public class ConnectionPool {
+
+    private LinkedList<Connection> pool = new LinkedList<Connection>();
+
+    public ConnectionPool(int initialSize) {
+        if (initialSize > 0) {
+            for (int i = 0; i < initialSize; i++) {
+                pool.addLast(ConnectionDriver.createConnection());
+            }
+        }
+    }
+
+    public void rerleaseConnection(Connection connection) {
+        if (connection != null) {
+            synchronized (pool) {
+                pool.add(connection);
+                pool.notifyAll();
+            }
+        }
+    }
+
+    public Connection fetchConnection(long mills) throws InterruptedException {
+        synchronized (pool) {
+            if (mills < 0) {
+                while (pool.isEmpty()) {
+                    pool.wait();
+                }
+                return pool.removeFirst();
+            } else {
+                long future = System.currentTimeMillis() + mills;
+                long remainMills = mills;
+                while (pool.isEmpty() && remainMills > 0) {
+                    pool.wait(remainMills);
+                    remainMills = future - System.currentTimeMillis();
+                }
+                Connection result = null;
+                if (!pool.isEmpty()) {
+                    result = pool.removeFirst();
+                }
+                return result;
+            }
+        }
+    }
+}
