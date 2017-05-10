2901fa37b5bbfa6fbbc008cda78faf70c695ae3e
jasonlee <libo529@gmail.com>
2017-05-10 20:15:27 +0800

    update path

--- java-collection/src/test/java/cn/lee/jason/thread/concurrent/demo/pool/simple/http/TestSimpleHttpServer.java
+++ java-collection/src/test/java/cn/lee/jason/thread/concurrent/demo/pool/simple/http/TestSimpleHttpServer.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class TestSimpleHttpServer {
     @Test
     public void testStart() throws IOException {
-        SimpleHttpServer.setBasePath("/home/jason/workbench/www/html/demo/extracting");
+        SimpleHttpServer.setBasePath("classpath://");
         SimpleHttpServer.start();
     }
 }
