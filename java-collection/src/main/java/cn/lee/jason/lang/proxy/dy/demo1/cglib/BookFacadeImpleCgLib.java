983a570d715ac7c049f1353747395f263094fadc
jasonlee529 <libo529@gmail.com>
2017-05-13 21:54:00 +0800

    add declear

--- java-collection/src/main/java/cn/lee/jason/lang/proxy/dy/demo1/cglib/BookFacadeImpleCgLib.java
+++ java-collection/src/main/java/cn/lee/jason/lang/proxy/dy/demo1/cglib/BookFacadeImpleCgLib.java
@@ -3,6 +3,7 @@ package cn.lee.jason.lang.proxy.dy.demo1.cglib;
 import cn.lee.jason.lang.proxy.dy.demo1.BookFacade;
 
 /**
+ * 基于CGlib实现的方法，其实和代理方式无关
  * Created by jason on 17-5-8.
  */
 public class BookFacadeImpleCgLib implements BookFacade {
