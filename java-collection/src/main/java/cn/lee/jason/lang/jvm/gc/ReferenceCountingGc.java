0b2d6f318b47fdb96df3b4e6035380d07e105872
jasonlee <libo529@gmail.com>
2017-06-20 17:59:14 +0800

    update

---
+++ java-collection/src/main/java/cn/lee/jason/lang/jvm/gc/ReferenceCountingGc.java
@@ -0,0 +1,26 @@
+package cn.lee.jason.lang.jvm.gc;
+
+/**
+ * Created by jason on 17-6-20.
+ */
+public class ReferenceCountingGc {
+
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void testGc() {
+        ReferenceCountingGc objectA = new ReferenceCountingGc();
+        ReferenceCountingGc objectB = new ReferenceCountingGc();
+        objectA.instance = objectB;
+        objectB.instance = objectA;
+        objectA = null;
+        objectB = null;
+        System.gc();
+
+    }
+    public static void main(String [] args){
+        testGc();
+    }
+}
+
